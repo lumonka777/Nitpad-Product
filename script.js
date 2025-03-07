b3fe7eba69ede82e86ae8b10cbcc094460f157db
lumonka777 <126884542+lumonka777@users.noreply.github.com>
2025-03-07 22:47:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function scrollToEnd() {
+    alert('Never gonna give you up, Never gonna let you down, Never gonna run around and desert you, Never gonna make you cry, Never gonna say goodbye, Never gonna a lieandhurtyou');
+}
